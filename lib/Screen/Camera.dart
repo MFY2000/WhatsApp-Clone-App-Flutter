7ecb8d1a11e9fe4d37e21bbd0560350109d053e9
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-19 20:46:41 +0500

    MFY auto commit at 19/10/2021 20:46:36

---
+++ lib/Screen/Camera.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+
+List<CameraDescription> cameras_ = []; 
+
+
+class Camera extends StatefulWidget {
+  const Camera({ Key? key }) : super(key: key);
+
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
