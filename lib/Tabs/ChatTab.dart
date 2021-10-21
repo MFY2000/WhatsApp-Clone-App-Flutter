7fe5f0ec52209ef7e7e20f9dcfdbb855ff869578
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-21 22:56:58 +0500

    Update ChatTab.dart

---
+++ lib/Tabs/ChatTab.dart
@@ -0,0 +1,66 @@
+
+// ignore_for_file: file_names
+
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/Model/ChatModel.dart';
+import 'package:whatsapp_clone/Screen/Chats_Screen.dart';
+
+class ChatTab extends StatefulWidget {
+  const ChatTab({ Key? key }) : super(key: key);
+
+  @override
+  _ChatTabState createState() => _ChatTabState();
+}
+
+class _ChatTabState extends State<ChatTab> {
+  List<ChatsModel> chats = [ ChatsModel(
+    name: "Muhammad Fahad", 
+    time: "7:23 pm", 
+    message: "Hi", 
+    tags: ["Pin","Mute","Unseen"], 
+    isGroup: false,
+    imageUrl: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg", 
+    chatStatus: "send" //Send {send, delivery, seen}, Receive 
+  ), ChatsModel(
+    name: "Muhammad Fahad", 
+    time: "7:23 pm", 
+    message: "Hi", 
+    tags: ["Unseen","Pin","Mute"], 
+    isGroup: false,
+    imageUrl: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg", 
+    chatStatus: "send" //Send {send, delivery, seen}, Receive 
+  ), ChatsModel(
+    name: "Muhammad Fahad", 
+    time: "7:23 pm", 
+    message: "Hi", 
+    tags: ["Unseen","Pin","Mute"], 
+    isGroup: false,
+    imageUrl: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg", 
+    chatStatus: "send" //Send {send, delivery, seen}, Receive 
+  ), ChatsModel(
+    name: "Muhammad Fahad", 
+    time: "7:23 pm", 
+    message: "Hi", 
+    tags: ["Unseen","Pin","Mute"], 
+    isGroup: false,
+    imageUrl: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg", 
+    chatStatus: "send" //Send {send, delivery, seen}, Receive 
+  ) 
+  
+   ]; 
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.chat),
+      ),
+      body: Container(
+        child: Chats_Screen(chats: chats),
+      ),
+    );
+  }
+}
