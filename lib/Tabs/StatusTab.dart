166fa3f4d0ab6fd56ace89c25e303ee0efca3d54
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-22 07:42:45 +0500

    Update StatusTab.dart

--- lib/Tabs/StatusTab.dart
+++ lib/Tabs/StatusTab.dart
@@ -2,6 +2,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/Custom%20UI/StatusCard.dart';
 
 class statusTab extends StatefulWidget {
   const statusTab({ Key? key }) : super(key: key);
@@ -15,20 +16,26 @@ class _statusTabState extends State<statusTab> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
         children: [
-           FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.chat),
-          ),
-          FloatingActionButton(
+           FloatingActionButton(         
             foregroundColor:Colors.black38,
             backgroundColor: Colors.grey[350], 
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap ,
+            onPressed: () {},
+            child: Icon(Icons.edit),
+          ),
+
+          SizedBox(height: 10,),
+
+          FloatingActionButton(
             onPressed: () {},
             child: Icon(Icons.camera_alt)
           ),
         ],
-      )
-      body:Conterain()
+      ),
+      body: Container(child: statusCard())
       
     );
   }
